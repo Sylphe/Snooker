c99f3e81141d86c076306f9ded5228b27d60410a
Sylphe <entropista@gmail.com>
2026-05-06 15:34:56 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,2 +1,2 @@
-// Snooker Practice Log v4.11.0.2.1 module bootstrap
-import "./modules/app-core.js?v=4.11.0";
+// Snooker Practice Log v4.14.1.2.1 module bootstrap
+import "./modules/app-core.js?v=4.14.1";
