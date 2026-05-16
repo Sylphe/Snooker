71fc02d38daddea91ddf25b17f353a66ceafd019
Sylphe <entropista@gmail.com>
2026-05-16 20:00:19 +0200

    v4.22.13

--- app.js
+++ app.js
@@ -1,2 +1,2 @@
-// Snooker Practice Log v4.22.12 module bootstrap
-import "./modules/app-core.js?v=4.22.12";
+// Snooker Practice Log v4.22.13 module bootstrap
+import "./modules/app-core.js?v=4.22.13";
