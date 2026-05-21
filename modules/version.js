658909fa426dafd1de81f76b24abcc63f3c4fdf0
Sylphe <entropista@gmail.com>
2026-05-21 19:07:53 +0200

    5.7.0 UX

--- modules/version.js
+++ modules/version.js
@@ -1,2 +1,2 @@
-export const APP_VERSION = "5.6.20-selective-library-import";
-export const APP_BUILD_TIMESTAMP = "2026-05-21 20:10 CEST";
+export const APP_VERSION = "5.7.0-focus-mode-ux-reset";
+export const APP_BUILD_TIMESTAMP = "2026-05-21 21:00 CEST";
