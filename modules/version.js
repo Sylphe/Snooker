f83b27eb175720ecce6153d036cf1f5107090746
Sylphe <entropista@gmail.com>
2026-05-29 15:00:48 +0200

    v5-7-77U-13-1-bootstrap-hydration-hotfix

--- modules/version.js
+++ modules/version.js
@@ -1,3 +1,5 @@
 export const APP_VERSION = "5.7.77U-13-ai-coaching-snapshot-reconciliation-fix";
 export const BUILD_DATE = "2026-05-29";
 export const APP_BUILD_LABEL = `Snooker Practice PWA ${APP_VERSION}`;
+
+export const APP_BUILD_TIMESTAMP = "2026-05-29 14:57 CEST";
